a87f3f41121fa4b2876bf6daa312660fe35cb2ef
Bekalu <bekpostgrad@gmail.com>
2024-10-03 06:33:39 -0500

    Evaluate ND update

--- IngestREST.py
+++ IngestREST.py
@@ -10,8 +10,8 @@
 #   1b) [DONE] Save the data to a CSV file to 'data\CO2\TotalEmissionsByState2019.csv'.
 #
 #
-#   2a) Code the REST API to get the 2019 state level total CO2 emissions for Coal.
-#   2b) Save the data to a CSV file to 'data\CO2\CoalEmissionsByState2019.csv'.
+#   2a) [DONE] Code the REST API to get the 2019 state level total CO2 emissions for Coal.
+#   2b) [DONE] Save the data to a CSV file to 'data\CO2\CoalEmissionsByState2019.csv'.
 #
 #   3) Answer the questions shown below by replacing YourAnswer and executing the code to save the file.
 
@@ -29,14 +29,15 @@ with open("Data/CO2/EvaluateND.CSV", "w") as file:
 
     # Question 1
     q1 = "What is the ND state rank in total emissions (high to low)? | " + \
-        "YourAnswer"
+        "low - ND ranks 34 out of 50 states in C02 emissions"
     file.write(q1 + "\n")
 
     # Question 2
     q2 = "What is the ND state rank in coal emissons (high to low)? | " + \
-        "YourAnswer"
+        "high - ND ranks 11 out of 50 states in CO2 emission from coal"
     file.write(q2 + "\n")
 
     # Question 3
-    q3 = "What is your perpective on how ND is doing with respect to CO2 emissions given the data above along with other insight you might have? | " + "YourAnswer"
+    q3 = "What is your perpective on how ND is doing with respect to CO2 emissions given the data above along with other insight you might have? | " + \
+        "Overall ND has low total CO2 emisson compared to states like Tx and Cali. But ND has high CO2 emission rank from coal, implying COAL mines are common in ND."
     file.write(q3 + "\n")
